99917e43172e64cb09801ad9e472fe1ae96b9c6b
Eduardo Henrique Bogoni <eduardo.bogoni@trf1.jus.br>
2020-02-20 17:30:29 -0300

    Ehbrs::Tools::Runner.

---
+++ lib/ehbrs/tools/runner.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+require 'eac_ruby_utils/console/docopt_runner'
+require 'ehbrs/tools/version'
+
+module Ehbrs
+  module Tools
+    class Runner < ::EacRubyUtils::Console::DocoptRunner
+      enable_console_speaker
+
+      DOC = <<~DOCOPT
+        Tools for EHB/RS.
+
+        Usage:
+          __PROGRAM__ [options] __SUBCOMMANDS__
+          __PROGRAM__ --version
+          __PROGRAM__ -h | --help
+
+        Options:
+          -h --help             Show this screen.
+          -V --version          Show version.
+      DOCOPT
+
+      def run
+        if options.fetch('--version')
+          out(::Ehbrs::Tools::VERSION + "\n")
+        else
+          run_with_subcommand
+        end
+      end
+    end
+  end
+end
