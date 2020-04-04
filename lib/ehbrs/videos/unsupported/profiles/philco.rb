cd7188fa6254db23aba9788d090a594e13105714
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-04-04 19:25:12 -0300

    Ehbrs::Videos::Unsupported::Profiles::Philco.

---
+++ lib/ehbrs/videos/unsupported/profiles/philco.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'ehbrs/videos/unsupported/profiles/base'
+
+module Ehbrs
+  module Videos
+    module Unsupported
+      module Profiles
+        class Philco < ::Ehbrs::Videos::Unsupported::Profiles::Base
+          AUDIO_SUPPORTED_CODECS = %w[aac ac3 eac3 mp3].freeze
+          AUDIO_UNSUPPORTED_CODECS = %w[dts].freeze
+
+          VIDEO_SUPPORTED_CODECS = %w[h264 mpeg4].freeze
+          VIDEO_UNSUPPORTED_CODECS = %w[hevc msmpeg4v3].freeze
+
+          SUBTITLE_SUPPORTED_CODECS = %w[ass dvd subrip].freeze
+          SUBTITLE_UNSUPPORTED_CODECS = %w[mov].freeze
+
+          OTHER_SUPPORTED_CODECS = %w[ttf].freeze
+
+          MPEG4_EXTRA_SUPPORTED = %w[xvid].freeze
+          MPEG4_EXTRA_UNSUPPORTED = %w[dx50].freeze
+        end
+      end
+    end
+  end
+end
