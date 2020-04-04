3ef132d251239edbbbd9b969ddc97d71a7def594
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-04-04 19:17:56 -0300

    Ehbrs::Videos::Unsupported::Checks::CodecUnsupported.

---
+++ lib/ehbrs/videos/unsupported/checks/codec_unsupported.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'ehbrs/videos/unsupported/fixes/supported_codec'
+
+module Ehbrs
+  module Videos
+    module Unsupported
+      module Checks
+        class CodecUnsupported
+          common_constructor :codec
+
+          def check(track)
+            return nil unless track.codec == codec
+
+            "Unsupported codec \"#{codec}\" for track #{track}"
+          end
+
+          def fix
+            ::Ehbrs::Videos::Unsupported::Fixes::SupportedCodec.new
+          end
+        end
+      end
+    end
+  end
+end
