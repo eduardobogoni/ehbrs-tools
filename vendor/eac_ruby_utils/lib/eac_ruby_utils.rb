778b1e61f96ccdcfde7af4c24368ccfe46aa1941
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-04-04 16:43:39 -0300

    Subrepo "vendor/eac_ruby_utils" (https://github.com/esquilo-azul/eac_ruby_utils).

---
+++ vendor/eac_ruby_utils/lib/eac_ruby_utils.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module EacRubyUtils
+  require 'eac_ruby_utils/arguments_consumer'
+  require 'eac_ruby_utils/configs'
+  require 'eac_ruby_utils/console'
+  require 'eac_ruby_utils/contextualizable'
+  require 'eac_ruby_utils/core_ext'
+  require 'eac_ruby_utils/envs'
+  require 'eac_ruby_utils/filesystem_cache'
+  require 'eac_ruby_utils/filesystem_traverser'
+  require 'eac_ruby_utils/fs_cache'
+  require 'eac_ruby_utils/listable'
+  require 'eac_ruby_utils/options_consumer'
+  require 'eac_ruby_utils/settings_provider'
+  require 'eac_ruby_utils/patch'
+  require 'eac_ruby_utils/patches'
+  require 'eac_ruby_utils/paths_hash'
+  require 'eac_ruby_utils/require_sub'
+  require 'eac_ruby_utils/rspec'
+  require 'eac_ruby_utils/simple_cache'
+  require 'eac_ruby_utils/yaml'
+end
---
+++ vendor/eac_ruby_utils/lib/eac_ruby_utils/common_constructor.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require 'active_support/callbacks'
+
+module EacRubyUtils
+  class CommonConstructor
+    attr_reader :args, :options
+
+    def initialize(*args)
+      @args = args
+    end
+
+    def setup_class(klass)
+      setup_class_attr_readers(klass)
+      setup_class_attr_writers(klass)
+      setup_class_initialize(klass)
+    end
+
+    def setup_class_attr_readers(klass)
+      klass.send(:attr_reader, *args)
+      klass.send(:public, *args)
+    end
+
+    def setup_class_attr_writers(klass)
+      klass.send(:attr_writer, *args)
+      klass.send(:private, *args.map { |a| "#{a}=" })
+    end
+
+    def setup_class_initialize(klass)
+      klass.include(::ActiveSupport::Callbacks)
+      klass.define_callbacks :initialize
+      klass.class_eval initialize_method_code, __FILE__, __LINE__
+    end
+
+    def initialize_method_code
+      b = "def initialize(#{initialize_method_args_code})\n"
+      b += "  run_callbacks :initialize do\n"
+      initialize_method_args.each do |arg|
+        b += "  self.#{arg} = #{arg}\n"
+      end
+      b += "  end\n"
+      b += "end\n"
+      b
+    end
+
+    def initialize_method_args_code
+      initialize_method_args.join(', ')
+    end
+
+    def initialize_method_args
+      args
+    end
+  end
+end
