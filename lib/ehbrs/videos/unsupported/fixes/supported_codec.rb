bf55854e95bcb7ca6fec7c47aca0455801ce5384
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-04-04 19:19:14 -0300

    Ehbrs::Videos::Unsupported::Fixes::SupportedCodec.

---
+++ lib/ehbrs/videos/unsupported/fixes/supported_codec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Ehbrs
+  module Videos
+    module Unsupported
+      module Fixes
+        class SupportedCodec
+          TRACK_TYPE_OPTIONS = {
+            'Audio' => '-acodec',
+            'Video' => '-vcodec',
+            'Subtitle' => '-scodec'
+          }.freeze
+
+          TRACK_TYPE_FIX_CODECS = {
+            'Audio' => 'aac',
+            'Video' => 'libx264',
+            'Subtitle' => 'ass'
+          }.freeze
+
+          def ffmpeg_args(track)
+            ["#{track_codec_option_by_type(track.type)}:#{track.number}",
+             track_codec_fix_by_type(track.type)]
+          end
+
+          def track_codec_option_by_type(track_type)
+            TRACK_TYPE_OPTIONS.fetch(track_type)
+          end
+
+          def track_codec_fix_by_type(track_type)
+            TRACK_TYPE_FIX_CODECS.fetch(track_type)
+          end
+        end
+      end
+    end
+  end
+end
