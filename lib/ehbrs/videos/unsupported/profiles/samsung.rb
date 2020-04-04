23faa5ad0f47448c8091d83e528651b1c4ad1a96
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-04-04 19:25:26 -0300

    Ehbrs::Videos::Unsupported::Profiles::Samsung.

---
+++ lib/ehbrs/videos/unsupported/profiles/samsung.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'ehbrs/videos/unsupported/checks/invalid_extension'
+require 'ehbrs/videos/unsupported/profiles/base'
+
+module Ehbrs
+  module Videos
+    module Unsupported
+      module Profiles
+        class Samsung < ::Ehbrs::Videos::Unsupported::Profiles::Base
+          AUDIO_SUPPORTED_CODECS = %w[aac ac3 dts eac3 mp3 vorbis].freeze
+          AUDIO_UNSUPPORTED_CODECS = %w[].freeze
+
+          VIDEO_SUPPORTED_CODECS = %w[h264 mpeg4 hevc].freeze
+          VIDEO_UNSUPPORTED_CODECS = %w[].freeze
+
+          SUBTITLE_SUPPORTED_CODECS = %w[ass dvd subrip].freeze
+          SUBTITLE_UNSUPPORTED_CODECS = %w[mov].freeze
+
+          OTHER_SUPPORTED_CODECS = %w[png ttf].freeze
+
+          MPEG4_EXTRA_SUPPORTED = %w[].freeze
+          MPEG4_EXTRA_UNSUPPORTED = %w[dx50 xvid].freeze
+
+          def initialize
+            add_check('invalid_extension', '.m4v')
+          end
+        end
+      end
+    end
+  end
+end
