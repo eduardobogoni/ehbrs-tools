eb742a6c23b5ab156b737a98e3da30ff88520d2d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-04-25 14:22:03 -0300

    Ehbrs::Executables.wit.

--- lib/ehbrs/executables.rb
+++ lib/ehbrs/executables.rb
@@ -16,7 +16,7 @@ module Ehbrs
 
       {
         '-version' => %w[ffmpeg ffprobe],
-        '--version' => %w[flips-linux]
+        '--version' => %w[flips-linux wit]
       }.each do |validate_arg, commands|
         commands.each do |command|
           define_method("#{command}_uncached") do
