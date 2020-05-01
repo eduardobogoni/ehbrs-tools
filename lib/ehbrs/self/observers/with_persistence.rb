d0b1b641632ef451580871bb79a814eb7a28c320
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-05-01 15:12:28 -0300

    Ehbrs::Self::Observers::WithPersistence.

---
+++ lib/ehbrs/self/observers/with_persistence.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'ehbrs/observers/with_persistence'
+require 'ehbrs/user_dirs'
+
+module Ehbrs
+  module Self
+    module Observers
+      class WithPersistence
+        enable_simple_cache
+        common_constructor :label
+
+        def check_current_value
+          observer.check(current_value)
+        end
+
+        def changing_value?
+          observer.changing_value?(current_value)
+        end
+
+        private
+
+        def current_value_uncached
+          calculate_value
+        end
+
+        def observer_uncached
+          ::Ehbrs::Observers::WithPersistence.new(persistence_path, blank_value: :raise)
+        end
+
+        def persistence_path_uncached
+          ::Ehbrs::UserDirs.data.child('observers', label.to_s.parameterize)
+                           .content_path.to_pathname
+        end
+      end
+    end
+  end
+end
