f58b673cc31845de550db63b19b822b2b0d64bae
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-05-01 19:14:04 -0300

    Ehbrs::UserDirs.

---
+++ lib/ehbrs/user_dirs.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/filesystem_cache'
+require 'eac_ruby_utils/simple_cache'
+
+module Ehbrs
+  module UserDirs
+    class << self
+      include ::EacRubyUtils::SimpleCache
+
+      def application_id
+        'ehbrs-tools'
+      end
+
+      private
+
+      def user_home_dir_uncached
+        ::EacRubyUtils::FilesystemCache.new(ENV['HOME'])
+      end
+
+      def cache_uncached
+        user_home_dir.child('.cache', application_id)
+      end
+
+      def config_uncached
+        user_home_dir.child('.config', application_id)
+      end
+
+      def data_uncached
+        user_home_dir.child('.local', 'share', application_id)
+      end
+    end
+  end
+end
