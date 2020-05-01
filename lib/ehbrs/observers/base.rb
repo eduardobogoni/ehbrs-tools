57a89f2365f183060dafc220b8ae042f19499ec2
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-05-01 15:11:43 -0300

    Ehbrs::Observers::Base.

---
+++ lib/ehbrs/observers/base.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+require 'ostruct'
+
+module Ehbrs
+  module Observers
+    class Base
+      include ::EacRubyUtils::Listable
+
+      lists.add_string :blank_value, :add, :ignore, :raise
+
+      attr_reader :records, :blank_value, :last_check_time
+
+      common_constructor :options, default: [{}] do
+        @records = options[:records] || []
+        @blank_value = options[:blank_value].if_present(BLANK_VALUE_ADD) do |v|
+          v = v.to_s
+          ::Ehbrs::Observers::Base.lists.blank_value.value_validate!(v)
+          v
+        end
+      end
+
+      def check(value, date = ::Time.zone.now)
+        @last_check_time = date
+        send("check_with_blank_value_#{blank_value}", value, date)
+      end
+
+      def changing_value?(value)
+        records.if_present(true) do
+          last_value.if_present(value.present?) { |v| v != value }
+        end
+      end
+
+      def last_change_time
+        records.last.if_present(&:time)
+      end
+
+      def last_value
+        records.last.if_present(&:value)
+      end
+
+      private
+
+      def check_with_blank_value_add(value, time)
+        return false unless changing_value?(value)
+
+        records << ::OpenStruct.new(value: value, time: time)
+        true
+      end
+
+      def check_with_blank_value_ignore(value, date)
+        return false if value.blank? ? false : check_with_blank_value_add(value, date)
+      end
+
+      def check_with_blank_value_raise(value, date)
+        raise(::ArgumentError, "Blank value checked (Value: #{value}, Class: #{value.class})") if
+        value.blank?
+
+        check_with_blank_value_add(value, date)
+      end
+    end
+  end
+end
---
+++ spec/lib/ehbrs/observers/base_spec.rb
@@ -0,0 +1,93 @@
+# frozen_string_literal: true
+
+require 'ehbrs/observers/base'
+
+RSpec.describe ::Ehbrs::Observers::Base do
+  let(:instance) { described_class.new }
+  let(:first_value) { 'a value' }
+  let(:same_value) { first_value.dup }
+  let(:different_value) { 'another value' }
+  let(:first_time) { ::Time.zone.local(2020, 5, 1, 12, 45, 0) }
+  let(:second_time) { ::Time.zone.local(2020, 5, 1, 12, 45, 0) }
+
+  it { expect(instance.records.count).to be_zero }
+  it { expect(instance.last_change_time).to be_blank }
+  it { expect(instance.last_check_time).to be_blank }
+
+  it { expect(instance).to be_changing_value(nil) }
+  it { expect(instance).to be_changing_value(first_value) }
+  it { expect(instance).to be_changing_value(same_value) }
+  it { expect(instance).to be_changing_value(different_value) }
+
+  context 'when firstly checked' do
+    before { instance.check(first_value, first_time) }
+
+    it { expect(instance.records.count).to eq(1) }
+    it { expect(instance.last_value).to eq(first_value) }
+    it { expect(instance).not_to be_changing_value(same_value) }
+    it { expect(instance).to be_changing_value(different_value) }
+    it { expect(instance.last_change_time).to eq(first_time) }
+    it { expect(instance.last_check_time).to eq(first_time) }
+
+    context 'when same first_value is checked' do
+      before { instance.check(same_value, second_time) }
+
+      it { expect(instance.records.count).to eq(1) }
+      it { expect(instance.last_value).to eq(same_value) }
+      it { expect(instance.last_change_time).to eq(first_time) }
+      it { expect(instance.last_check_time).to eq(second_time) }
+    end
+
+    context 'when different first_value is checked' do
+      before { instance.check(different_value, second_time) }
+
+      it { expect(instance.records.count).to eq(2) }
+      it { expect(instance.last_value).to eq(different_value) }
+      it { expect(instance.last_change_time).to eq(second_time) }
+      it { expect(instance.last_check_time).to eq(second_time) }
+    end
+
+    context 'when checked first_value is blank' do
+      context 'when blank_value == :add' do
+        let(:instance) { described_class.new(blank_value: :add) }
+
+        before { instance.check(nil, second_time) }
+
+        it { expect(instance.records.count).to eq(2) }
+        it { expect(instance.last_value).to eq(nil) }
+        it { expect(instance.last_change_time).to eq(second_time) }
+        it { expect(instance.last_check_time).to eq(second_time) }
+      end
+
+      context 'when blank_value == :ignore' do
+        let(:instance) { described_class.new(blank_value: :ignore) }
+
+        before { instance.check(nil, second_time) }
+
+        it { expect(instance.records.count).to eq(1) }
+        it { expect(instance.last_value).to eq(first_value) }
+        it { expect(instance.last_change_time).to eq(first_time) }
+        it { expect(instance.last_check_time).to eq(second_time) }
+      end
+
+      context 'when blank_value == :raise' do
+        let(:instance) { described_class.new(blank_value: :raise) }
+
+        before do
+          @exception = nil
+          begin
+            instance.check(nil, second_time)
+          rescue StandardError => e
+            @exception = e
+          end
+        end
+
+        it { expect(@exception).to be_a(::ArgumentError) } # rubocop:disable RSpec/InstanceVariable
+        it { expect(instance.records.count).to eq(1) }
+        it { expect(instance.last_value).to eq(first_value) }
+        it { expect(instance.last_change_time).to eq(first_time) }
+        it { expect(instance.last_check_time).to eq(second_time) }
+      end
+    end
+  end
+end
