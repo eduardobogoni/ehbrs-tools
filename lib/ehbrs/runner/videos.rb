04ff98b1aac1758a722c859f54665c8843356119
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-09-27 15:22:31 -0300

    Ehbrs::Runner::Videos: refatora com EacCli.

--- lib/ehbrs/runner/videos.rb
+++ lib/ehbrs/runner/videos.rb
@@ -1,23 +1,18 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils/core_ext'
+require 'eac_cli/core_ext'
 require 'eac_ruby_utils/console/docopt_runner'
 
 module Ehbrs
   class Runner < ::EacRubyUtils::Console::DocoptRunner
     class Videos < ::EacRubyUtils::Console::DocoptRunner
+      runner_with :output_file
       require_sub __FILE__
 
-      DOC = <<~DOCOPT
-        Video tools for EHB/RS.
-
-        Usage:
-          __PROGRAM__ [options] __SUBCOMMANDS__
-          __PROGRAM__ -h | --help
-
-        Options:
-          -h --help             Show this screen.
-      DOCOPT
+      runner_definition do
+        desc 'Video tools for EHB/RS.'
+        subcommands
+      end
     end
   end
 end
