499bceb9b9de4797bb9e3f590e15f57d7243d45d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-11-12 20:25:09 -0300

    Gem "eac_cli" (Local): atualiza para '0.9.0'.

--- vendor/eac_cli/lib/eac_cli/runner/context.rb
+++ vendor/eac_cli/lib/eac_cli/runner/context.rb
@@ -5,13 +5,30 @@ require 'eac_ruby_utils/core_ext'
 module EacCli
   module Runner
     class Context
-      attr_reader :argv, :parent, :program_name
+      attr_reader :argv, :parent, :program_name, :runner
 
-      def initialize(*context_args)
+      def initialize(runner, *context_args)
         options = context_args.extract_options!
         @argv = (context_args[0] || options.delete(:argv) || ARGV).dup.freeze
         @parent = context_args[1] || options.delete(:parent)
         @program_name = options.delete(:program_name)
+        @runner = runner
+      end
+
+      # Call a method in the runner or in one of it ancestors.
+      def call(method_name, *args)
+        return runner.send(method_name, *args) if runner.respond_to?(method_name)
+        return parent_call(method_name, *args) if parent.present?
+
+        raise ::NameError, "No method \"#{method_name}\" found in #{runner} or in its ancestors"
+      end
+
+      protected
+
+      def parent_call(method_name, *args)
+        return parent.context(method_name, *args) if parent.respond_to?(:context)
+
+        parent.runner_context.call(method_name, *args)
       end
     end
   end
