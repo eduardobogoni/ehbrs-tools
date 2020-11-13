16ab9823578afba5c3671c068a0a2d58f0d3a1fc
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-11-12 22:12:11 -0300

    Ehbrs::Runner::Self::Test: dispensa EacRubyUtils::Console::DocoptRunner.

--- lib/ehbrs/runner/self/test.rb
+++ lib/ehbrs/runner/self/test.rb
@@ -1,19 +1,15 @@
 # frozen_string_literal: true
 
-require 'eac_cli/default_runner'
+require 'eac_cli/core_ext'
 require 'ehbrs/gems'
 require 'eac_ruby_gems_utils/tests/multiple'
-require 'eac_ruby_utils/core_ext'
 require 'eac_ruby_utils/console/docopt_runner'
 
 module Ehbrs
   class Runner < ::EacRubyUtils::Console::DocoptRunner
     class Self < ::EacRubyUtils::Console::DocoptRunner
-      class Test < ::EacRubyUtils::Console::DocoptRunner
-        require_sub __FILE__
-        include ::EacCli::DefaultRunner
-
-        runner_definition do
+      class Test
+        runner_with :help do
           desc 'Test core and local gems.'
         end
 
