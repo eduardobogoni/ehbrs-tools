dfa7f0cb79cc613ba380affd5f1d4d25cc2b85b4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-11-15 11:18:23 -0300

    Ehbrs::Runner::Videos: remove opção não utilizada "--output-file".

--- lib/ehbrs/runner/videos.rb
+++ lib/ehbrs/runner/videos.rb
@@ -6,10 +6,9 @@ require 'eac_ruby_utils/console/docopt_runner'
 module Ehbrs
   class Runner < ::EacRubyUtils::Console::DocoptRunner
     class Videos < ::EacRubyUtils::Console::DocoptRunner
-      runner_with :output_file
       require_sub __FILE__
 
-      runner_definition do
+      runner_with do
         desc 'Video tools for EHB/RS.'
         subcommands
       end
