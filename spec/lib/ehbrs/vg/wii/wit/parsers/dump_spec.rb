f95157c73dc41d3673251cf924a405b9da009689
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-11-20 13:31:05 -0300

    RSpec.describe(Ehbrs::Vg::Wii::Wit::Parsers::Dump).

---
+++ spec/lib/ehbrs/vg/wii/wit/parsers/dump_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'aranha/parsers/spec/source_target_fixtures_example'
+require 'ehbrs/vg/wii/wit/parsers/dump'
+
+RSpec.describe ::Ehbrs::Vg::Wii::Wit::Parsers::Dump do
+  include_examples 'source_target_fixtures', __FILE__
+
+  def source_data(source_file)
+    described_class.new(::File.read(source_file)).properties
+  end
+end
