8b767a19ed85848b8b50fff21f3ea0afc27f2672
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-11-30 23:02:48 -0300

    Gem "aranha-parsers" (Local): atualiza para '0.7.0'.

--- vendor/aranha-parsers/lib/aranha/parsers/spec/source_target_fixtures_example.rb
+++ vendor/aranha-parsers/lib/aranha/parsers/spec/source_target_fixtures_example.rb
@@ -20,8 +20,8 @@ RSpec.shared_examples 'source_target_fixtures' do |spec_file| # rubocop:disable 
         source_target_fixtures.source_files.each do |source_file|
           sd = sort_results(source_data(source_file))
           basename = ::Aranha::Parsers::SourceTargetFixtures.source_target_basename(source_file)
-          target_file = File.expand_path("../#{basename}.target.yaml", source_file)
-          File.write(target_file, sd.to_yaml)
+          target_file = File.expand_path("../#{basename}.target#{target_file_extname}", source_file)
+          File.write(target_file, target_content(sd))
         end
       end
     else
@@ -66,5 +66,13 @@ RSpec.shared_examples 'source_target_fixtures' do |spec_file| # rubocop:disable 
 
   def target_data(target_file)
     ::YAML.load_file(target_file)
+  end
+
+  def target_content(data)
+    data.to_yaml
+  end
+
+  def target_file_extname
+    '.yaml'
   end
 end
