699983ab9caa6573e09afad2141b6a6d82ba4746
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-11-30 23:39:53 -0300

    RSpec.describe(Ehbrs::Runner::Videos::Unsupported): testa formato resultante.

--- spec/lib/ehbrs/runner/videos/unsupported_spec.rb
+++ spec/lib/ehbrs/runner/videos/unsupported_spec.rb
@@ -3,6 +3,7 @@
 require 'ehbrs/runner'
 require 'eac_ruby_utils/fs/temp'
 require 'ehbrs/runner/videos/unsupported'
+require 'ehbrs_ruby_utils/videos/container'
 
 RSpec.describe ::Ehbrs::Runner::Videos::Unsupported do
   let(:source_dir) { ::Pathname.new(__dir__).expand_path / 'unsupported_spec_files' }
@@ -30,6 +31,16 @@ RSpec.describe ::Ehbrs::Runner::Videos::Unsupported do
         let(:run_argv) { ['videos', 'unsupported', '-f', temp_dir.to_s] }
         let(:converted_file) { to_fix_file.basename_sub { |b| "#{b}.converted" } }
         let(:fixed_file) { to_fix_file.basename_sub { |b| "#{b.basename('.*')}.mkv" } }
+        let(:fixed_file_actual_probe_data) do
+          ::EhbrsRubyUtils::Videos::Container.new(fixed_file).probe_data
+        end
+
+        let(:fixed_file_expect_probe_file) { source_dir.join("#{video_var}.probe.yaml") }
+        let(:fixed_file_expect_probe_data) do
+          ::EacRubyUtils::Yaml.load(fixed_file_expect_probe_file.read).deep_merge(
+            format: { filename: fixed_file.to_path }
+          )
+        end
 
         before do
           ::Ehbrs::Runner.run(argv: run_argv)
@@ -37,6 +48,10 @@ RSpec.describe ::Ehbrs::Runner::Videos::Unsupported do
 
         it { expect(converted_file).to be_a_file }
         it { expect(fixed_file).to be_a_file }
+
+        it 'has expected probe data' do
+          expect(fixed_file_actual_probe_data).to eq(fixed_file_expect_probe_data)
+        end
       end
     end
   end
