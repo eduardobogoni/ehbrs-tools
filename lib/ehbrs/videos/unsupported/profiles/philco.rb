81ac14e97d1af766d02e4202541f410ca8df1af2
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-12-08 10:23:18 -0300

    Ehbrs::Videos::Unsupported::Profiles::Philco::SUBTITLE_SUPPORTED_CODECS: adiciona "dvd_subtitle".

--- lib/ehbrs/videos/unsupported/profiles/philco.rb
+++ lib/ehbrs/videos/unsupported/profiles/philco.rb
@@ -13,7 +13,7 @@ module Ehbrs
           VIDEO_SUPPORTED_CODECS = %w[h264 mpeg4].freeze
           VIDEO_UNSUPPORTED_CODECS = %w[hevc msmpeg4v3].freeze
 
-          SUBTITLE_SUPPORTED_CODECS = %w[ass dvd subrip].freeze
+          SUBTITLE_SUPPORTED_CODECS = %w[ass dvd dvd_subtitle subrip].freeze
           SUBTITLE_UNSUPPORTED_CODECS = %w[mov].freeze
 
           OTHER_SUPPORTED_CODECS = %w[png ttf].freeze
