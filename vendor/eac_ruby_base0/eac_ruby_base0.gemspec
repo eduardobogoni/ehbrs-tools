b26be6651e47104c040ca2e88c9b8bc7aab03a9e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-12-30 09:46:23 -0300

    Gem "eac_ruby_base0" (Local): adiciona '0.3.1'.

---
+++ vendor/eac_ruby_base0/eac_ruby_base0.gemspec
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.push File.expand_path('lib', __dir__)
+
+require 'eac_ruby_base0/version'
+
+Gem::Specification.new do |s|
+  s.name        = 'eac_ruby_base0'
+  s.version     = EacRubyBase0::VERSION
+  s.authors     = ['Put here the authors']
+  s.summary     = 'Put here de description.'
+
+  s.files = Dir['{lib}/**/*']
+
+  s.add_dependency 'eac_cli', '~> 0.7'
+  s.add_dependency 'eac_ruby_gems_utils', '~> 0.7', '>= 0.7.2'
+  s.add_dependency 'eac_ruby_utils', '~> 0.46'
+
+  s.add_development_dependency 'eac_ruby_gem_support', '~> 0.1', '>= 0.1.2'
+end
---
+++ vendor/eac_ruby_base0/lib/eac_ruby_base0/runner.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'eac_cli/default_runner'
+require 'eac_ruby_utils/core_ext'
+
+module EacRubyBase0
+  module Runner
+    require_sub __FILE__
+    common_concern do
+      include ::EacCli::DefaultRunner
+      runner_definition do
+        bool_opt '-q', '--quiet', 'Quiet mode.'
+        bool_opt '-I', '--no-input', 'Fail if a input is requested.'
+        subcommands
+        alt do
+          bool_opt '-V', '--version', 'Show version.', usage: true, required: true
+        end
+      end
+    end
+
+    def run
+      on_speaker_node do |node|
+        node.stderr = ::StringIO.new if options.fetch('--quiet')
+        node.stdin = FailIfRequestInput.new if options.fetch('--no-input')
+        if options.fetch('--version')
+          show_version
+        else
+          run_with_subcommand
+        end
+      end
+    end
+
+    def application_version
+      context(:application).version.to_s
+    end
+
+    def show_version
+      out("#{application_version}\n")
+    end
+
+    class FailIfRequestInput
+      enable_console_speaker
+
+      %w[gets noecho].each do |method|
+        define_method(method) do
+          raise "Input method requested (\"#{method}\") and option --no-input is set"
+        end
+      end
+    end
+  end
+end
