d140722fc66a87f9550d4091b11e83b63913042c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-02-06 15:12:16 -0300

    Ehbrs::CookingBook::Project.recipes: lê arquivos somente do subdiretório "/recipes/".

--- lib/ehbrs/cooking_book/project.rb
+++ lib/ehbrs/cooking_book/project.rb
@@ -6,6 +6,8 @@ require 'ehbrs/cooking_book/recipe'
 module Ehbrs
   module CookingBook
     class Project
+      RECIPES_ROOT_SUBPATH = 'recipes'
+
       enable_simple_cache
       common_constructor :root do
         self.root = root.to_pathname
@@ -15,9 +17,13 @@ module Ehbrs
 
       private
 
+      def recipes_root_uncached
+        root.join(RECIPES_ROOT_SUBPATH)
+      end
+
       def recipes_uncached
-        ::Dir.glob(File.join('**', '*.{yml,yaml}'), base: root.to_path).map do |subpath|
-          ::Ehbrs::CookingBook::Recipe.from_file(root.join(subpath))
+        ::Dir.glob(File.join('**', '*.{yml,yaml}'), base: recipes_root.to_path).map do |subpath|
+          ::Ehbrs::CookingBook::Recipe.from_file(recipes_root.join(subpath))
         end
       end
     end
