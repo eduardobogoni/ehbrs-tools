0715735adae2db40532908f279c00e2b44441c77
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-02-20 12:41:24 -0300

    Ehbrs::Runner::Music.

---
+++ lib/ehbrs/runner/music.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'eac_cli/core_ext'
+
+module Ehbrs
+  class Runner
+    class Music
+      require_sub __FILE__
+
+      runner_with :help, :subcommands do
+        desc 'Ferramentas para músicas.'
+        subcommands
+      end
+    end
+  end
+end
