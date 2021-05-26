b02fa9cf1db22191472d34cddb3c6bf2bee40136
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-05-25 22:29:04 -0300

    spec/**/*.rb: adequa a "eac_ruby_gem_support" '0.2.0'.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,9 @@ require 'tmpdir'
 
 RSpec.configure do |config|
   config.example_status_persistence_file_path = ::File.join(::Dir.tmpdir, 'ehbrs-tools_rspec')
+
+  require 'eac_ruby_gem_support/rspec'
+  ::EacRubyGemSupport::Rspec.setup(::File.expand_path('..', __dir__), config)
 end
 
 require 'eac_ruby_utils/core_ext'
