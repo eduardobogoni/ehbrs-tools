6bdae48751eb00cae4fad9d59a26b025181e2068
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-09-21 23:59:41 -0300

    Ehbrs::Tools::Runner::Fs::Selected: refatora com EhbrsRubyUtils::Fs::Selected.

--- lib/ehbrs/tools/runner/fs/selected.rb
+++ lib/ehbrs/tools/runner/fs/selected.rb
@@ -1,24 +1,25 @@
 # frozen_string_literal: true
 
 require 'eac_ruby_utils/envs'
-require 'eac_ruby_utils/fs/clearable_directory'
+
 require 'ehbrs/tools/core_ext'
+require 'ehbrs_ruby_utils/fs/selected'
 
 module Ehbrs
   module Tools
     class Runner
       class Fs
         class Selected
-          DEFAULT_TRAVERSER_RECURSIVE = true
-
           enable_jobs_runner
-          runner_with :confirmation, :help, :filesystem_traverser do
+          runner_with :confirmation, :help do
             desc 'Seleciona diretórios.'
             arg_opt '-b', '--build-dir', 'Constrói diretório alvo.'
+            pos_arg :root_path
           end
 
           def run
-            infov 'Recursive', traverser_recursive
+            infov 'Root path', selected.root_path
+            infov 'Build directory', build_dir.if_present('-')
             run_jobs :show, :build
           end
 
@@ -33,24 +34,18 @@ module Ehbrs
           end
 
           def directory_target_basename(directory)
-            directory.basename.to_path
+            ::EhbrsRubyUtils::Fs::Selected::Build::DEFAULT_TARGET_BASENAME_PROC.call(directory)
           end
 
           private
 
           def build
             infom "Building directory \"#{build_dir}\" with selected..."
-            build_dir.clear
-            selected_directories.each do |directory|
-              ::EacRubyUtils::Envs.local.command(
-                'ln', '-s', directory.to_path,
-                build_dir.join(directory_target_basename(directory))
-              ).system!
-            end
+            selected.build(build_dir, &method(:directory_target_basename)).perform
           end
 
           def build_dir_uncached
-            parsed.build_dir.if_present { |v| ::EacRubyUtils::Fs::ClearableDirectory.new(v) }
+            parsed.build_dir.if_present(&:to_pathname)
           end
 
           def run_build?
@@ -59,9 +54,11 @@ module Ehbrs
 
           def selected_directories_uncached
             infom 'Searching selected directories...'
-            @selected = []
-            run_filesystem_traverser
-            @selected.sort
+            selected.found.map { |path| build_selected_directory(path) }
+          end
+
+          def selected_uncached
+            ::EhbrsRubyUtils::Fs::Selected.new(parsed.root_path)
           end
 
           def show
@@ -69,10 +66,6 @@ module Ehbrs
             selected_directories.each do |directory|
               puts directory_label(directory)
             end
-          end
-
-          def traverser_check_directory(path)
-            @selected << build_selected_directory(path) if path.join('.selected').exist?
           end
         end
       end
