1286ddb6024dcbf166efdfb5fd28c6d507689820
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-09-22 20:08:08 -0300

    Object.erb_template: substitui por "eac_templates":Object.erb_template.

--- lib/ehbrs/patches/object/erb_template.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'ehbrs/patches/module/erb_template'
-
-class Object
-  def erb_template(subpath, binding_obj = self)
-    self.class.erb_template(subpath, binding_obj)
-  end
-end
