213a6fab7cd32f64bca6362224271f4b05ffd3ce
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-02-08 20:00:30 -0300

    Gem "eac_fs" (Local): atualiza para '0.11.1'.

--- sub/eac_fs/lib/eac_fs/cached_download.rb
+++ sub/eac_fs/lib/eac_fs/cached_download.rb
@@ -3,17 +3,17 @@
 require 'eac_fs/patches'
 require 'eac_ruby_utils/fs/temp'
 
-module Avm
+module EacFs
   class CachedDownload
     attr_reader :url, :fs_cache
 
     def initialize(url, parent_fs_cache = nil)
       @url = url
-      @fs_cache = (parent_fs_cache || fs_cache).child(url.parameterize)
+      @fs_cache = (parent_fs_cache || fs_cache).child(url.to_s.parameterize)
     end
 
     def assert
-      download unless fs_cache.cached?
+      download unless fs_cache.stored?
     end
 
     def download
