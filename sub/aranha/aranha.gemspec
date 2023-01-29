727a912aeaeb131ff0ff76004fb257184e7e36eb
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-01-29 01:12:08 -0300

    Gem "aranha" (Local): adiciona '0.18.0'.

---
+++ sub/aranha/aranha.gemspec
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.push File.expand_path('lib', __dir__)
+
+# Maintain your gem's version:
+require 'aranha/version'
+
+# Describe your gem and declare its dependencies:
+Gem::Specification.new do |s|
+  s.name        = 'aranha'
+  s.version     = Aranha::VERSION
+  s.authors     = ['Eduardo H. Bogoni']
+  s.email       = ['eduardobogoni@gmail.com']
+  s.summary     = 'Ruby utilities for web crawling.'
+  s.license     = 'MIT'
+
+  s.files = Dir['{app,config,db,lib}/**/*', 'MIT-LICENSE', 'README.rdoc']
+  s.test_files = Dir['test/**/*']
+
+  s.add_dependency 'aranha-parsers', '~> 0.14', '>= 0.14.3'
+  s.add_dependency 'aranha-selenium', '~> 0.5'
+  s.add_dependency 'eac_ruby_utils', '~> 0.106', '>= 0.106.1'
+  s.add_dependency 'httpclient', '~> 2.8', '>= 2.8.3'
+
+  s.add_development_dependency 'eac_ruby_gem_support', '~> 0.3', '>= 0.3.1'
+end
---
+++ sub/aranha/lib/aranha/temporary_errors_manager.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+require 'eac_ruby_utils/gems_registry'
+
+module Aranha
+  module TemporaryErrorsManager
+    GEMS_REGISTRY_MODULE_SUFFIX = 'TemporaryErrors'
+
+    class << self
+      enable_simple_cache
+
+      # @return [Exception]
+      def errors
+        errors_providers.flat_map(&:errors)
+      end
+
+      # @return [Array]
+      def errors_providers
+        gems_registry.registered.map(&:registered_module)
+      end
+
+      # @param error Exception
+      # @return [Boolean]
+      def temporary_error?(error)
+        return true if errors.any? { |klass| error.is_a?(klass) }
+
+        error.cause.present? ? temporary_error?(error.cause) : false
+      end
+
+      private
+
+      # @return [EacRubyUtils::GemsRegistry]
+      def gems_registry_uncached
+        ::EacRubyUtils::GemsRegistry.new(GEMS_REGISTRY_MODULE_SUFFIX)
+      end
+    end
+  end
+end
