18a0089d5d87a7c08544760777373c2c881a9683
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-09-30 12:18:14 -0300

    ehbrs/patches/object/template.rb: remove.

--- lib/ehbrs/patches/object/template.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'ehbrs/tools/application'
-require 'eac_templates/patches/object/template'
-
-::EacTemplates::Searcher.default.included_paths <<
-  ::Ehbrs::Tools.application.gemspec_dir.join('template').to_path
