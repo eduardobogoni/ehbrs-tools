bb987a3b0eba84f5aae5820039490768e33a9a6a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-11-25 23:31:16 -0300

    Gem "eac_cli" (Local): adiciona '0.39.0'.

---
+++ sub/eac_cli/lib/eac_cli/runner_with/output_list/base_formatter.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+require 'eac_ruby_utils/acts_as_abstract'
+
+module EacCli
+  module RunnerWith
+    module OutputList
+      class BaseFormatter
+        acts_as_abstract :to_output
+        common_constructor :columns, :rows
+
+        # @return [String]
+        def build_column(column)
+          column.to_s
+        end
+
+        # @return [Array<String>]
+        def build_columns
+          columns.map(&:to_s)
+        end
+
+        # @param row [Object]
+        # @return [Object]
+        def build_row(_row)
+          raise_abstract_method __method__
+        end
+
+        # @return [Array<Hash<String, String>>]
+        def build_rows
+          rows.map { |row| build_row(row) }
+        end
+      end
+    end
+  end
+end
