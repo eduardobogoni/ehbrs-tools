9091dde7fc5dcc8da3aea6da30539237e8b42e99
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-05-15 21:19:53 -0300

    Ehbrs::Tools::Runner::Music::Sort::Load: converte de "ehbrs_ruby_utils"#EhbrsRubyUtils::Music::Sort::Commands::Load.

---
+++ lib/ehbrs/tools/runner/music/sort/load.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_base0/core_ext'
+
+module Ehbrs
+  module Tools
+    class Runner
+      class Music
+        class Sort
+          class Load
+            runner_with :help, :confirmation
+
+            def run
+              info "Reading \"#{path}\"..."
+              config = build_config
+              s = config.to_yaml
+              puts s
+              if confirm?
+                info("Writing to \"#{config_file}\"...")
+                File.write(config_file, s)
+              end
+              puts 'Done!'.green
+            end
+
+            private
+
+            def build_config
+              config = {}
+              scanner.by_section.each do |section, fs|
+                config[section] = fs.sort.map(&:name)
+              end
+              config
+            end
+          end
+        end
+      end
+    end
+  end
+end
