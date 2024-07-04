72a1aae0e545309265763f5d2a291a40761aa4b5
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-07-04 01:32:07 -0300

    RSpec.describe(Ehbrs::Tools::Runner::Vg::Ips): facilita o teste de outros tipos de patch.

--- spec/lib/ehbrs/tools/runner/vg/ips_spec.rb
+++ spec/lib/ehbrs/tools/runner/vg/ips_spec.rb
@@ -6,27 +6,35 @@ require 'ehbrs/tools/runner/vg/ips'
 
 RSpec.describe Ehbrs::Tools::Runner::Vg::Ips do
   include_context 'spec_paths', __FILE__
-  let(:source_dir) { fixtures_directory.expand_path __dir__ }
-  let(:source_file) { source_dir / 'source.rom' }
-  let(:patches) { 2.times.map { |i| source_dir / "patch_#{i}.ips" } }
 
   describe '#run' do
-    let(:output_file) { EacRubyUtils::Fs::Temp.file }
-    let(:expected_file) { source_dir / 'expected.rom' }
-    let(:run_argv) do
-      ['vg', 'ips', '--output-file', output_file.to_path, source_file.to_path] +
-        patches.map(&:to_path)
-    end
+    %w[ips].each do |patch_extname|
+      context "when the patches have extname \"#{patch_extname}\"" do
+        let(:source_dir) { fixtures_directory.expand_path __dir__ }
+        let(:source_file) { source_dir / 'source.rom' }
+        let(:patches) { 2.times.map { |i| source_dir / "patch_#{i}.#{patch_extname}" } }
 
-    before do
-      Ehbrs::Tools::Runner.run(argv: run_argv)
-    end
+        let(:output_file) { EacRubyUtils::Fs::Temp.file }
+        let(:expected_file) { source_dir / 'expected.rom' }
+        let(:run_argv) do
+          ['vg', 'ips', '--output-file', output_file.to_path, source_file.to_path] +
+            patches.map(&:to_path)
+        end
 
-    after do
-      output_file.unlink if output_file.exist?
-    end
+        before do
+          Ehbrs::Tools::Runner.run(argv: run_argv)
+        end
 
-    it { expect(output_file).to exist }
-    it { expect(FileUtils.compare_file(output_file.to_path, expected_file.to_path)).to be_truthy }
+        after do
+          output_file.unlink if output_file.exist?
+        end
+
+        it { expect(output_file).to exist }
+
+        it do
+          expect(FileUtils.compare_file(output_file.to_path, expected_file.to_path)).to be_truthy
+        end
+      end
+    end
   end
 end
