3741e3dfa91457472a8b7f640dae298e6ef3ce68
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-07-03 23:25:40 -0300

    Ehbrs::Tools::Vg::Patches.

---
+++ lib/ehbrs/tools/vg/patches.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+
+module Ehbrs
+  module Tools
+    module Vg
+      module Patches
+        require_sub __FILE__
+      end
+    end
+  end
+end
