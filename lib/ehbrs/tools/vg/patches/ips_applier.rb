e86322df16f917e91d64254501169d4a15511771
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-07-04 00:25:35 -0300

    Ehbrs::Tools::Vg::Patches::BaseApplier: extrai de Ehbrs::Tools::Vg::Patches::IpsApplier.

--- lib/ehbrs/tools/vg/patches/ips_applier.rb
+++ lib/ehbrs/tools/vg/patches/ips_applier.rb
@@ -2,28 +2,13 @@
 
 require 'eac_ruby_utils/core_ext'
 require 'ehbrs/tools/executables'
+require 'ehbrs/tools/vg/patches/base_applier'
 
 module Ehbrs
   module Tools
     module Vg
       module Patches
-        class IpsApplier
-          common_constructor :patch_path
-
-          # @param source_path [Pathname]
-          # @param output_path [Pathname]
-          # @raise [RuntimeError] Se o arquivo de saída não é criado.
-          def apply(source_path, output_path)
-            source_path = source_path.to_pathname
-            output_path = output_path.to_pathname
-            command(source_path, output_path).system!
-
-            return if output_path.exist?
-
-            fatal_error("Applier returned without error, but output file \"#{output_path}\"" \
-                        'does not exist')
-          end
-
+        class IpsApplier < ::Ehbrs::Tools::Vg::Patches::BaseApplier
           # @param source_path [Pathname]
           # @param output_path [Pathname]
           # @return [EacRubyUtils::Envs::Command]
