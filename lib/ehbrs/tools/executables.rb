5c65e4731e056e0bbd055f36cd1cff44529493cb
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-06-25 09:18:32 -0300

    Ehbrs::Tools::Executables.*: move to "ehbrs_ruby_utils"#EhbrsRubyUtils::Executables.{flips,xdelta3}.

--- lib/ehbrs/tools/executables.rb
+++ lib/ehbrs/tools/executables.rb
@@ -9,19 +9,6 @@ module Ehbrs
         def env
           ::EacRubyUtils::Envs.local
         end
-
-        private
-
-        {
-          '--version' => %w[flips],
-          '-V' => %w[xdelta3]
-        }.each do |validate_arg, commands|
-          commands.each do |command|
-            define_method("#{command}_uncached") do
-              env.executable(command, validate_arg)
-            end
-          end
-        end
       end
     end
   end
