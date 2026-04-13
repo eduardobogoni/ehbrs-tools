0e098fda742c0748ecbb4c968db74e4a5c6dbeb3
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-04-12 21:54:12 -0300

    Sub[sub/ultimate_lyrics]: remove.

--- sub/ultimate_lyrics/lib/ultimate_lyrics.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'eac_ruby_utils'
-EacRubyUtils::RootModuleSetup.perform __FILE__
-
-module UltimateLyrics
-end
-
-require 'aranha/parsers'
--- sub/ultimate_lyrics/spec/spec_helper.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-require 'ultimate_lyrics'
-require 'eac_ruby_gem_support'
-EacRubyUtils::Rspec.default_setup_create(File.expand_path('..', __dir__))
