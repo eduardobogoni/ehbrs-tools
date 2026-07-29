c22128b7b8739e59d753307beb087c180350a12a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-07-29 17:39:47 -0300

    EacEnvs::Http: refact with EacRubyBase1::RootModuleSetup.

--- sub/eac_envs-http/lib/eac_envs/http.rb
+++ sub/eac_envs-http/lib/eac_envs/http.rb
@@ -1,17 +1,12 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils'
-EacRubyUtils::RootModuleSetup.perform __FILE__
-
-module EacEnvs
-  module Http
-  end
+require 'eac_ruby_base1'
+EacRubyBase1::RootModuleSetup.perform __FILE__ do
+  require 'eac_fs'
+  require 'faraday'
+  require 'faraday/follow_redirects'
+  require 'faraday/gzip'
+  require 'faraday/multipart'
+  require 'faraday/retry'
+  require 'json'
 end
-
-require 'eac_fs'
-require 'faraday'
-require 'faraday/follow_redirects'
-require 'faraday/gzip'
-require 'faraday/multipart'
-require 'faraday/retry'
-require 'json'
