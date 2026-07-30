c2f04c3a985478adaf6704e59ca6286420e54393
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-07-29 21:42:19 -0300

    Ehbrs::Tools: refatora com EacRubyBase1::RootModuleSetup.

--- lib/ehbrs/tools.rb
+++ lib/ehbrs/tools.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils'
-EacRubyUtils::RootModuleSetup.perform __FILE__
+require 'eac_ruby_base1'
+EacRubyBase1::RootModuleSetup.perform __FILE__ do
+  require 'avm/tools'
+  require 'eac_cli'
+  require 'eac_fs'
+  require 'eac_ruby_base0'
+  require 'ehbrs_ruby_utils'
+end
 
 module Ehbrs
   module Tools
     include ::Ehbrs::Tools::Application
   end
 end
-
-require 'avm/tools'
-require 'eac_cli'
-require 'eac_fs'
-require 'eac_ruby_base0'
-require 'ehbrs_ruby_utils'
